1803575838568490f9ef90cd3c16c13c8a1edd88
Jonathan <jonathancarranza70@gmail.com>
2023-12-24 18:31:58 -0300

    curso de dalto 48min

--- INICIO/variables.js
+++ INICIO/variables.js
@@ -1,10 +1,17 @@
-var altura = 10;
-var base = 20;
-let perimetro;
-perimetro = 2*altura + 2*base;
+//variables en Javascript
+ string = 'esto es un string';
+ numbre = 35;
+ booleano = true; //o false, paso señal o no paso señal, 0 o 1.
 
-var dia="Lunes";
-function verDia(){
-console.log("Hoy es ", dia);
-}
-verDia();
+//var let y const   var tiene valor global, const no se puede cambiar, let es local
+let numero2;
+numero2 = 29;
+//podemos declararla en la misma linea
+let numero = 29;
+
+//undefind  no esta definida ni inicializada
+//null esta vacio, es un valor vacio y esta inicializada
+//Nan   not a number
+
+let nombre = prompt('¿Decime tu nombre?')
+alert('hola ' + nombre)
